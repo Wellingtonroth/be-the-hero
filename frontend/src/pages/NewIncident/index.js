9eedbbf712f25a7712ef31a19e9cf7c9c834760b
Wellington Roth <wellington_roth@hotmail.com>
2020-03-25 20:46:10 -0300

    Profile Page

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
+import logoImg from "../../assets/logo.svg";
+import "./styles.css";
+
+export default function NewIncident() {
+  return <h1>New Incidents</h1>
+}
